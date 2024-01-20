f5243675b49a572085ce8673e64acda27c645229
julianlliu <julianlliu@tencent.com>
2024-01-20 20:13:35 +0800

    feat: add logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import { NAVIGATION_LIST } from './utils';
+
 import styles from './index.module.css';
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <Link href="/">OpenSand</Link>
+        <Link href="/">
+          <Image width={100} height={100} className={styles.image} src="/OpenSand.png" alt="OpenSand" />
+        </Link>
       </div>
       <nav className={styles.navigation}>
         {NAVIGATION_LIST.map((item, index) => (
