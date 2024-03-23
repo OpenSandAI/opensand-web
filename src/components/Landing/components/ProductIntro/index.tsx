3ec8dd1af99810c490c074dba90413a722f7fd67
Shawn Tian <hightian@gmail.com>
2024-03-23 20:04:50 +0800

    update product intro

--- src/components/Landing/components/ProductIntro/index.tsx
+++ src/components/Landing/components/ProductIntro/index.tsx
@@ -22,22 +22,19 @@ const ProductIntro: FC<ProductIntroProps> = props => {
                 color: '#FFF',
               }}
             >
-              <span>BUILD IT</span>
-              <br />
-              <span style={{ marginLeft: '10rem', color: 'rgba(0, 230, 137, 1)' }}>YOUR</span> WAY
+              Sand <span style={{ color: 'rgba(0, 230, 137, 1)' }}>Consensus</span>
             </Typography>
           </Grid>
           <Grid item xs={12} md={4}>
-            <Box sx={{textAlign: { xs: 'center', md: '' },}}>
-            <Image
-              width={300}
-              height={300}
-              className={styles.image}
-              src="/images/site/landing/landing-secondary.png"
-              alt="landing-logo"
-            />
+            <Box sx={{ textAlign: { xs: 'center', md: '' } }}>
+              <Image
+                width={300}
+                height={300}
+                className={styles.image}
+                src="/images/site/landing/landing-secondary.png"
+                alt="landing-logo"
+              />
             </Box>
-           
           </Grid>
           <Grid item xs={12} md={8}>
             <Box sx={{ mt: { xs: 5, md: 0 } }}>
@@ -49,7 +46,7 @@ const ProductIntro: FC<ProductIntroProps> = props => {
                   fontSize: '2rem',
                 }}
               >
-                Cosmo Intellect
+                AI-based Proof of Work (AI-MPoW)
               </Typography>
               <Typography
                 component="div"
@@ -59,11 +56,9 @@ const ProductIntro: FC<ProductIntroProps> = props => {
                   fontSize: '1.2rem',
                 }}
               >
-                {`n this phase of the product, we will focus on building an open-source community where decision-making
-                authority and knowledge resources are shared with a broader group. It's a place where professionals from
-                various disciplines, including technology, academia, industry, and everyday enthusiasts, come together
-                to pool their wisdom to collectively explore and address complex AGI governance issues, ensuring a more
-                balanced and comprehensive understanding and management of AGI.`}
+                {`The Sand consensus of OpenSand integrates AI-based Proof of Work (AI-MPoW) and Proof of Stake (AI-GPoS) mechanisms, constructing an efficient, secure, and decentralized hybrid consensus model.
+AI-driven adaptive incentives and dynamic staking mechanisms achieve coordination of computational power and stakeholder interests. Moreover, it enables decentralized services by AI models at the consensus layer.
+Zero-knowledge proofs and smart contract verification ensure privacy and security. The Sand consensus sets new benchmarks in fairness, scalability, and security, injecting vibrant energy into the development of blockchain technology.`}
               </Typography>
               <div className={styles.operation}>
                 <a className={styles.button} href="/apps">
